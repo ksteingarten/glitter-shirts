a84c29e26dd97b201748148c49efb3de73661424
ksteingarten <85062889+ksteingarten@users.noreply.github.com>
2024-02-28 04:09:48 -0800

    Set up domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({  
-  site: 'https://ksteingarten.github.io',
-base: '/glitter-shirts',
+  site: 'https://designs.joshuastarr.ca',
 });
